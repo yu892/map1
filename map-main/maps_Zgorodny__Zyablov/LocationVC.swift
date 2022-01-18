c79d566db82e58563581c18229e3864e05a3a2f3
Leoka <74716100+yu892@users.noreply.github.com>
2022-01-18 15:23:20 +0300

    Add files via upload

---
+++ map-main/maps_Zgorodny__Zyablov/LocationVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maps_Zgorodny_Zyablov
+//
+//  Created by Даниил on 20.12.2021.
+//
+
+import UIKit
+
+class LocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ map-main/maps_Zgorodny__ZyablovTests/maps_Zgorodny__ZyablovTests.swift
@@ -0,0 +1,33 @@
+//
+//  maps_Zgorodny__ZyablovTests.swift
+//  maps_Zgorodny_ZyablovTests
+//
+//  Created by Даниил on 20.12.2021.
+//
+
+import XCTest
+@testable import maps_Zgorodny__Zyablov
+
+class maps_Zgorodny__ZyablovTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
